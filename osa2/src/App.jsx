78feb39b2ba4c2b0db9a8413d0912faf09dd7c8c
Konsta <Konsta.Launonen@student.oulu.fi>
2026-06-02 09:01:38 +0300

    2.18-2.19

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -1,168 +1,65 @@
 import { useState, useEffect } from 'react'
-import numberService from './services/Numbers.jsx'
+import countryService from './services/Countries.jsx'
 import axios from 'axios'
 import './index.css'
 
 const Filter = ( {filter, setFilter} ) => {
   const handleFilterChange = (event) => {
     console.log(event.target.value)
-    setFilter(event.target.value)
+    setFilter(event.target.value.toLowerCase())
   }
+
+  return (
+    <div>
+      find countries <input placeholder="find countries..." onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const Countries = ( {countries, setCountries, filter, setFilter} ) => {
+  const handleShow = (event) => {
+    setFilter(event.target.parentNode.firstChild.textContent.toLowerCase())
+  }
+  
+  countryService.getAll().then(initialCountries => {
+      setCountries(initialCountries)
+    }
+  )
+  const showCountries = countries.filter(country => country.name.common.toLowerCase().includes(filter))
+  if (showCountries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+  else if (1 < showCountries.length && showCountries.length < 10) {
+    return <div>{showCountries.map(country => <p key={country.name.common}>{country.name.common}<button onClick={handleShow}>show</button></p>)}</div>
+  }
+  else if (showCountries.length === 1) {
+    const country = showCountries[0]
 
     return (
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange} />
-      </div>
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
+      <h1>Languages</h1>
+      <ul>
+        {Object.values(country.languages).map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} />
+    </div>
     )
   }
-
-const Notification = ({ message, color }) => {
-  if (message === null) {
-    return null
-  }
-
-  return (
-    <div className="error" style={{ color: color }}>
-      {message}
-    </div>
-  )
 }
-
-const PersonForm = ( {newName, newNumber, setNewName, setNewNumber, persons, setPersons, setMessage, setColor} ) => {
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: newNumber
-    }
-    if (persons.some(person => person.name === newName)) {
-      if (!window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        return
-      }
-      const person = persons.find(person => person.name === newName)
-      const updatedPerson = {...person, number: newNumber}
-      numberService.update(persons.find(person => person.name === newName).id, personObject)
-        .then(response => {
-          setPersons(persons.map(p => p.id !== person.id ? p : updatedPerson))
-        })
-      .catch(error => {
-        setColor('red')
-        setMessage('Failed to update number')
-        setTimeout(() => {
-          setMessage(null)
-        }, 5000)
-      })
-      setColor('green')
-      setMessage(`Updated ${newName}'s number`)
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-      setNewName('')
-      setNewNumber('')
-      return
-    }
-    numberService.create(personObject)
-     .then(response => {
-      setPersons(persons.concat(response))
-      setMessage(`Added ${response.name}`)
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-      setNewName('')
-      setNewNumber('')
-    })
-    .catch(error => {
-      setColor('red')
-      setMessage('Failed to add person')
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    })
-  }
-  return (
-    <form onSubmit={addPerson}>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-      </div>
-      <div>
-        number: <input value={newNumber} onChange={handleNumberChange}/>
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  )
-}
-
-const Persons = ( {persons, filter, setPersons, setMessage, setColor} ) => {
-  const handleDelete = (id) => {
-    if (!window.confirm('Delete this person?')) {
-      return
-    }
-    numberService.remove(id)
-     .then(response => {
-      console.log(`Deleted ${response.name}`)
-      setColor('green')
-      setMessage(`Deleted ${response.name}`)
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
-    })
-      .catch(error => {
-        setColor('red')
-        setMessage('Failed to delete person')
-        setTimeout(() => {
-          setMessage(null)
-        }, 5000)
-      })
-    setPersons(persons.filter(person => person.id !== id))
-  }
-
-  return (
-    <div>
-      {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}<button onClick={() => handleDelete(person.id)}>delete</button></p>)}
-    </div>
-  )
-}
-
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-  const [message, setMessage] = useState(null)
-  const [color, setColor] = useState('green')
-
-  useEffect(() => {
-  console.log('effect')
-  numberService.getAll()
-    .then(data => {
-      console.log('promise fulfilled')
-      setPersons(data)
-    })
-}, [])
-
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState(null)
 
   return (
     <div>
-      <h2>Phonebook</h2>
       <Filter filter={filter} setFilter={setFilter} />
-      <h2>Add new</h2>
-      <PersonForm newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber} persons={persons} setPersons={setPersons} setMessage={setMessage} setColor={setColor} />
-      <Notification className="error" message={message} color={color} />
-      <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} setPersons={setPersons} setMessage={setMessage} setColor={setColor} />
+      <Countries countries={countries} setCountries={setCountries} filter={filter} setFilter={setFilter} />
     </div>
   )
 }
